3d841789c40c7259686bf62e902537d07a2159fc
Kudoshi <brendenguan@live.com>
2021-08-15 21:14:09 +0800

    Left right bottom up movement works now

--- main.py
+++ main.py
@@ -6,7 +6,6 @@
 '''
 import random
 
-
 def setup():
     global boardList
     boardList = []
@@ -96,43 +95,102 @@ def movementDown():
             if element[0] == searchRow and element[1] == col:
                 return True
     #Set movement values for the loops
+    movement = "Left"
+    if movement == "Down":
+        colRange = [0,gridSize,1] #Update sequence left to right
+        rowRange = [gridSize-2, -1, -1] #Comparison grid move up
+        searchRowRange = [1, gridSize, 1] #Search down. Current row +1 get down
+    elif movement == "Top":
+        colRange = [0, gridSize, 1]  # Left to right col
+        rowRange = [1, gridSize, 1]  # Start from second from top, go down
+        searchRowRange = [-1, -1, -1]  # Search up.  Current row - 1 get up.
+    elif movement == "Left":
+        colRange = [1,gridSize,1] #Comparison grid move right
+        rowRange = [0,gridSize,1] #Update sequence top to bottom
+        searchColRange = [-1,-1,-1] #Search left. Current col - 1 get left
+    elif movement == "Right":
+        colRange = [gridSize-2,-1,-1] #Comparison grid move left
+        rowRange = [0,gridSize,1] #Update sequence top to bottom
+        searchColRange = [1, gridSize, 1] #Search right Current col +1 get up
 
-    for col in range(0, gridSize):
-        #For each row before the end of grid. Descending order
-        print("Column Number: ", col)
-        lockedGridList = []  # [ [3,1],[]]
-        for row in range(gridSize-2, -1, -1):
-            #Check if comparingGrid is empty
-            if boardList[row][col] == 0:
-                continue
 
-            currentEmptyGrid = None
-            comparingGridCombined = False
+    if movement == "Left" or movement == "Right":
+        for row in range(rowRange[0], rowRange[1], rowRange[2]):
+            print("Row Number: ", row)
+            lockedGridList = []
 
-
-            #Search through the grids below
-            for searchRow in range(row+1, gridSize,1):
-                #Check if the searched grid is in the lockedGridList
-                if checkLockedGrid(searchRow,col) == True:
-                    break
-
-                #Check if the searched grid is empty
-                if boardList[searchRow][col] == 0:
-                    currentEmptyGrid = [searchRow,col]
+            for col in range(colRange[0], colRange[1], colRange[2]):
+                # Check if comparingGrid is empty
+                if boardList[row][col] == 0:
                     continue
-                #Check if searchGrid's value is matches the comparingGrid
-                elif boardList[searchRow][col] == boardList[row][col]:
-                    comparingGridCombined = True
-                    result = boardList[searchRow][col] + boardList[row][col]
-                    boardList[searchRow][col] = result
+
+                currentEmptyGrid = None
+                comparingGridCombined = False
+
+                # Search through the grids below
+                for searchCol in range(col + searchColRange[0], searchColRange[1], searchColRange[2]):
+                    # Check if the searched grid is in the lockedGridList
+                    if checkLockedGrid(row, searchCol) == True:
+                        break
+
+                    # Check if the searched grid is empty
+                    if boardList[row][searchCol] == 0:
+                        currentEmptyGrid = [row, searchCol]
+                        continue
+                    # Check if searchGrid's value is matches the comparingGrid
+                    elif boardList[row][searchCol] == boardList[row][col]:
+                        comparingGridCombined = True
+                        result = boardList[row][searchCol] + boardList[row][col]
+                        boardList[row][searchCol] = result
+                        boardList[row][col] = 0
+
+                        lockedGridList.append([row, searchCol])
+                    else:
+                        break
+
+                # Check if no combination was made
+                if comparingGridCombined == False and currentEmptyGrid != None:
+                    boardList[currentEmptyGrid[0]][currentEmptyGrid[1]] = boardList[row][col]
                     boardList[row][col] = 0
 
-                    lockedGridList.append([searchRow,col])
+    if movement == "Top" or movement == "Down":
+        for col in range(colRange[0], colRange[1], colRange[2]):
+            #For each row before the end of grid. Descending order
+            # print("Column Number: ", col)
+            lockedGridList = []  # [ [3,1],[]]
+            for row in range(rowRange[0], rowRange[1], rowRange[2]):
+                #Check if comparingGrid is empty
+                if boardList[row][col] == 0:
+                    continue
 
-            #Check if no combination was made
-            if comparingGridCombined == False and currentEmptyGrid != None:
-                boardList[currentEmptyGrid[0]][currentEmptyGrid[1]] = boardList[row][col]
-                boardList[row][col] = 0
+                currentEmptyGrid = None
+                comparingGridCombined = False
+
+                #Search through the grids below
+                for searchRow in range(row + searchRowRange[0], searchRowRange[1],searchRowRange[2]):
+                    #Check if the searched grid is in the lockedGridList
+                    if checkLockedGrid(searchRow,col) == True:
+                        break
+
+                    #Check if the searched grid is empty
+                    if boardList[searchRow][col] == 0:
+                        currentEmptyGrid = [searchRow,col]
+                        continue
+                    #Check if searchGrid's value matches the comparingGrid
+                    elif boardList[searchRow][col] == boardList[row][col]:
+                        comparingGridCombined = True
+                        result = boardList[searchRow][col] + boardList[row][col]
+                        boardList[searchRow][col] = result
+                        boardList[row][col] = 0
+
+                        lockedGridList.append([searchRow,col])
+                    else: #Break searching if encounters a value that doesn't match.
+                        break
+
+                #Check if no combination was made
+                if comparingGridCombined == False and currentEmptyGrid != None:
+                    boardList[currentEmptyGrid[0]][currentEmptyGrid[1]] = boardList[row][col]
+                    boardList[row][col] = 0
 
 
 
@@ -148,7 +206,10 @@ for i in range(1,100,1):
     print("current loop: ",i)
 
     if checkEmptyGrid() == True:
+        print("ADDING ONE VALUE")
         addOneValue()
+        displayBoard()
+        print("MOVEMENT DOWN")
         movementDown()
         displayBoard()
     else:
